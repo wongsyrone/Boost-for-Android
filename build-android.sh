565e9924784752bed6c3aea30b0aa4e2037af1bd
Syrone Wong <wong.syrone@gmail.com>
2020-08-18 22:35:54 +0800

    add boost 1.74.0
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- build-android.sh
+++ build-android.sh
@@ -30,12 +30,16 @@ SCRIPTDIR="$(cd "$(dirname "$0")"; pwd)" # " # This extra quote fixes syntax hig
 # -----------------------
 
 BOOST_VER1=1
-BOOST_VER2=73
+BOOST_VER2=74
 BOOST_VER3=0
-register_option "--boost=<version>" boost_version "Boost version to be used, one of {1.73.0, 1.70.0, 1.69.0, 1.68.0, 1.67.0, 1.66.0, 1.65.1, 1.55.0, 1.54.0, 1.53.0, 1.49.0, 1.48.0, 1.45.0}, default is 1.73.0."
+register_option "--boost=<version>" boost_version "Boost version to be used, one of {1.74.0, 1.73.0, 1.70.0, 1.69.0, 1.68.0, 1.67.0, 1.66.0, 1.65.1, 1.55.0, 1.54.0, 1.53.0, 1.49.0, 1.48.0, 1.45.0}, default is 1.73.0."
 boost_version()
 {
-  if [ "$1" = "1.73.0" ]; then
+  if [ "$1" = "1.74.0" ]; then
+    BOOST_VER1=1
+    BOOST_VER2=74
+    BOOST_VER3=0
+  elif [ "$1" = "1.73.0" ]; then
     BOOST_VER1=1
     BOOST_VER2=73
     BOOST_VER3=0
@@ -492,34 +496,36 @@ then
     cat $BOOST_DIR/tools/build/src/user-config-python.jam >> $BOOST_DIR/tools/build/src/user-config.jam
   fi
 
-  for dir in $PATCH_BOOST_DIR; do
-    if [ ! -d "$dir" ]; then
-      echo "Could not find directory '$dir' while looking for patches"
-      exit 1
-    fi
-
-    PATCHES=`(cd $dir && ls *.patch | sort) 2> /dev/null`
-
-    if [ -z "$PATCHES" ]; then
-      echo "No patches found in directory '$dir'"
-      exit 1
-    fi
-
-    for PATCH in $PATCHES; do
-      PATCH=`echo $PATCH | sed -e s%^\./%%g`
-      SRC_DIR=$PROGDIR/$BOOST_DIR
-      PATCHDIR=`dirname $PATCH`
-      PATCHNAME=`basename $PATCH`
-      log "Applying $PATCHNAME into $SRC_DIR/$PATCHDIR"
-      cd $SRC_DIR && patch -p1 < $dir/$PATCH && cd $PROGDIR
-      if [ $? != 0 ] ; then
-        dump "ERROR: Patch failure !! Please check your patches directory!"
-        dump "       Try to perform a clean build using --clean ."
-        dump "       Problem patch: $dir/$PATCHNAME"
-        exit 1
-      fi
-    done
-  done
+  echo "do the patch manually"
+  read
+  #for dir in $PATCH_BOOST_DIR; do
+  #  if [ ! -d "$dir" ]; then
+  #    echo "Could not find directory '$dir' while looking for patches"
+  #    exit 1
+  #  fi
+  #
+  #  PATCHES=`(cd $dir && ls *.patch | sort) 2> /dev/null`
+  #
+  #  if [ -z "$PATCHES" ]; then
+  #    echo "No patches found in directory '$dir'"
+  #    exit 1
+  #  fi
+  #
+  #  for PATCH in $PATCHES; do
+  #    PATCH=`echo $PATCH | sed -e s%^\./%%g`
+  #    SRC_DIR=$PROGDIR/$BOOST_DIR
+  #    PATCHDIR=`dirname $PATCH`
+  #    PATCHNAME=`basename $PATCH`
+  #    log "Applying $PATCHNAME into $SRC_DIR/$PATCHDIR"
+  #    cd $SRC_DIR && patch -p1 < $dir/$PATCH && cd $PROGDIR
+  #    if [ $? != 0 ] ; then
+  #      dump "ERROR: Patch failure !! Please check your patches directory!"
+  #      dump "       Try to perform a clean build using --clean ."
+  #      dump "       Problem patch: $dir/$PATCHNAME"
+  #      exit 1
+  #    fi
+  #  done
+  #done
 fi
 
 echo "# ---------------"
